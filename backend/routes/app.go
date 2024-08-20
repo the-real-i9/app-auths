2c7a415d832121301a67753e0cd88256842084c3
i9 <oluwarinolasam@gmail.com>
2024-08-20 14:35:16 +0100

    restricted resource test

--- backend/routes/app.go
+++ backend/routes/app.go
@@ -1,10 +1,12 @@
 package routes
 
 import (
+	"i9codesauths/backend/helpers"
 	"os"
 
 	jwtware "github.com/gofiber/contrib/jwt"
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
 )
 
 func App(router fiber.Router) {
@@ -14,6 +16,19 @@ func App(router fiber.Router) {
 
 	// access a restricted resource : jwt auth
 	router.Get("/restricted", func(c *fiber.Ctx) error {
-		return c.JSON(c.Locals("user"))
+		type User struct {
+			Id       int    `json:"id"`
+			Email    string `json:"email"`
+			Username string `json:"username"`
+		}
+
+		user := c.Locals("user").(*jwt.Token)
+		claims := user.Claims.(jwt.MapClaims)
+
+		var userdata User
+
+		helpers.MapToStruct(claims["data"].(map[string]any), &userdata)
+
+		return c.JSON(userdata)
 	})
 }
