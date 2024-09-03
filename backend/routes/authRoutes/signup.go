dec08cc4b56127380be9e0c217f2765c7c327d15
i9 <oluwarinolasam@gmail.com>
2024-09-03 15:26:12 +0100

    changing route name from submit_email to request_new_account

--- backend/routes/authRoutes/signup.go
+++ backend/routes/authRoutes/signup.go
@@ -1,14 +1,14 @@
 package authRoutes
 
 import (
-	"i9codesauths/backend/handlers/signupHandlers"
+	"appauths/backend/handlers/signupHandlers"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func Signup(router fiber.Router) {
 	// a cookie restricted to this path is used to maintain a session through the signup process
-	router.Post("/submit_email", signupHandlers.SubmitEmail)   // issue cookie associated with the next state info
-	router.Post("/verify_email", signupHandlers.VerifyEmail)   // verify cookie and state, update cookie for next state
-	router.Post("/register_user", signupHandlers.RegisterUser) // verify cookie and state, destroy cookie
+	router.Post("/request_new_account", signupHandlers.RequestNewAccount) // issue cookie associated with the next state info
+	router.Post("/verify_email", signupHandlers.VerifyEmail)              // verify cookie and state, update cookie for next state
+	router.Post("/register_user", signupHandlers.RegisterUser)            // verify cookie and state, destroy cookie
 }
