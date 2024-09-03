5669d3f12beba96270613db4cdd8df305d3155f3
i9 <oluwarinolasam@gmail.com>
2024-09-03 15:27:50 +0100

    replacing jwtware with the normal jwt.Verify procedure

--- backend/routes/app.go
+++ backend/routes/app.go
@@ -1,33 +1,31 @@
 package routes
 
 import (
-	"i9codesauths/backend/helpers"
+	"appauths/backend/appTypes"
+	"appauths/backend/helpers"
 	"os"
+	"strings"
 
-	jwtware "github.com/gofiber/contrib/jwt"
 	"github.com/gofiber/fiber/v2"
-	"github.com/golang-jwt/jwt/v5"
 )
 
 func App(router fiber.Router) {
-	router.Use(jwtware.New(jwtware.Config{
-		SigningKey: jwtware.SigningKey{Key: []byte(os.Getenv("AUTH_JWT_SECRET"))},
-		ContextKey: "auth",
-	}))
+	router.Use(func(c *fiber.Ctx) error {
+		authToken := strings.Fields(c.GetReqHeaders()["Authorization"][0])[1]
+
+		user, err := helpers.JwtVerify[appTypes.User](authToken, os.Getenv("AUTH_JWT_SECRET"))
+		if err != nil {
+			return c.Status(fiber.StatusUnauthorized).SendString(err.Error())
+		}
+
+		c.Locals("user", user)
+
+		return c.Next()
+	})
 
 	// access a restricted resource : jwt auth
 	router.Get("/restricted", func(c *fiber.Ctx) error {
-		type User struct {
-			Id       int    `json:"id"`
-			Email    string `json:"email"`
-			Username string `json:"username"`
-		}
-
-		userMap := c.Locals("auth").(*jwt.Token).Claims.(jwt.MapClaims)["data"].(map[string]any)
-
-		var user User
-
-		helpers.MapToStruct(userMap, &user)
+		user := c.Locals("user").(*appTypes.User)
 
 		return c.JSON(user)
 	})
