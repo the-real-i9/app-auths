b3bb8079d7078f2ec2f7488ebae14594978ebf4a
i9 <oluwarinolasam@gmail.com>
2024-09-06 00:09:22 +0100

    modified DDL

--- appauths_database_backup.sql
+++ appauths_database_backup.sql
@@ -29,7 +29,10 @@ CREATE TABLE public.auth_user (
     email character varying NOT NULL,
     username character varying NOT NULL,
     password character varying NOT NULL,
-    totp_setup_key character varying
+    totp_setup_key character varying,
+    mfa_enabled boolean DEFAULT false NOT NULL,
+    mfa_type character varying,
+    CONSTRAINT mfa_type_options CHECK ((((mfa_type)::text = 'otp'::text) OR ((mfa_type)::text = 'totp'::text)))
 );
 
 
@@ -71,6 +74,19 @@ CREATE TABLE public.ongoing_auth (
 ALTER TABLE public.ongoing_auth OWNER TO i9;
 
 --
+-- Name: ongoing_process; Type: TABLE; Schema: public; Owner: i9
+--
+
+CREATE TABLE public.ongoing_process (
+    k character varying(64) DEFAULT ''::character varying NOT NULL,
+    v bytea NOT NULL,
+    e bigint DEFAULT '0'::bigint NOT NULL
+);
+
+
+ALTER TABLE public.ongoing_process OWNER TO i9;
+
+--
 -- Name: auth_user user_id; Type: DEFAULT; Schema: public; Owner: i9
 --
 
@@ -81,7 +97,7 @@ ALTER TABLE ONLY public.auth_user ALTER COLUMN user_id SET DEFAULT nextval('publ
 -- Data for Name: auth_user; Type: TABLE DATA; Schema: public; Owner: i9
 --
 
-COPY public.auth_user (user_id, email, username, password, totp_setup_key) FROM stdin;
+COPY public.auth_user (user_id, email, username, password, totp_setup_key, mfa_enabled, mfa_type) FROM stdin;
 \.
 
 
@@ -94,10 +110,33 @@ COPY public.ongoing_auth (k, v, e) FROM stdin;
 
 
 --
+-- Data for Name: ongoing_process; Type: TABLE DATA; Schema: public; Owner: i9
+--
+
+COPY public.ongoing_process (k, v, e) FROM stdin;
+\.
+
+
+--
 -- Name: auth_user_user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: i9
 --
 
 SELECT pg_catalog.setval('public.auth_user_user_id_seq', 1, false);
+
+
+--
+-- Name: ongoing_process ongoing_process_pkey; Type: CONSTRAINT; Schema: public; Owner: i9
+--
+
+ALTER TABLE ONLY public.ongoing_process
+    ADD CONSTRAINT ongoing_process_pkey PRIMARY KEY (k);
+
+
+--
+-- Name: e; Type: INDEX; Schema: public; Owner: i9
+--
+
+CREATE INDEX e ON public.ongoing_auth USING btree (e);
 
 
 --
