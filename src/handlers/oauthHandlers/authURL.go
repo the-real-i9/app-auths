18fc4c012365989452e9eb4afd0237df9e9b3b76
i9 <oluwarinolasam@gmail.com>
2024-09-07 23:22:08 +0100

    github auth url

--- src/handlers/oauthHandlers/authURL.go
+++ src/handlers/oauthHandlers/authURL.go
@@ -31,5 +31,27 @@ func GoogleAuthURL(c *fiber.Ctx) error {
 	}
 
 	return c.SendString(fmt.Sprintf("Visit the URL for the auth dialog: %v", url))
-	// return c.Redirect(url)
+}
+
+func GithubAuthURL(c *fiber.Ctx) error {
+
+	clientId := os.Getenv("GITHUB_OAUTH_CLIENT_ID")
+	redirectURI := "http://127.0.0.1:5000/api/auth/oauth/github/callback"
+	scope := "user:email"
+	state := helpers.JwtSign("oauth: github callback", os.Getenv("SESSION_JWT_SECRET"), time.Now().Add(24*time.Hour))
+
+	url := fmt.Sprintf("https://github.com/login/oauth/authorize?client_id=%s&redirect_uri=%s&scope=%s&state=%s", clientId, redirectURI, scope, state)
+
+	session, err := globalVars.AuthSessionStore.Get(c)
+	if err != nil {
+		panic(err)
+	}
+
+	session.Set("state", "oauth: github callback")
+
+	if err := session.Save(); err != nil {
+		panic(err)
+	}
+
+	return c.SendString(fmt.Sprintf("Visit the URL for the auth dialog: %v", url))
 }
